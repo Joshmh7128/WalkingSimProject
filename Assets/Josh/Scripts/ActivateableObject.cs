83f206d9a7215a47f4dc99b813704da06d2ddb37
Josh <joshmh7128@gmail.com>
2021-10-18 16:44:02 -0400

    Basics of abstract classes

---
+++ Assets/Josh/Scripts/ActivateableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActivateableObject : MonoBehaviour
+{
+    // all puzzle object parameters go in here
+    public abstract void ActivateObject();
+}
