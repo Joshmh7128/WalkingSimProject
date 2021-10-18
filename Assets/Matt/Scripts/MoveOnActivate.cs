d9bcccff911e1f246105b99e7534844b49ec83df
Matt <mw4854@nyu.edu>
2021-10-18 18:36:41 -0400

    New scene; Activatable bridge and half arch prefabs

---
+++ Assets/Matt/Scripts/MoveOnActivate.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnActivate : ActivateableObject
+{
+    [SerializeField] Vector3 startOffset;
+
+    [SerializeField] bool moveRelative;
+    [SerializeField] Vector3 movement;
+    [SerializeField] Transform moveTo;
+
+    [SerializeField] float moveOverTime;
+
+    Vector3 originalPosition;
+    Vector3 destination;
+    float timer;
+
+    private void Start()
+    {
+        originalPosition = transform.position + startOffset;
+        ActivateObject();
+    }
+
+    public override void ActivateObject()
+    {
+        if (moveRelative)
+        {
+            destination = transform.position + movement;
+        }
+        else
+        {
+            if (moveTo != null)
+            {
+                destination = moveTo.position + movement;
+            }
+            else
+            {
+                destination = movement;
+            }
+        }
+        timer = 0;
+
+        StartCoroutine(MoveRoutine());
+    }
+
+    IEnumerator MoveRoutine()
+    {
+        while (timer < moveOverTime)
+        {
+            transform.position = Vector3.Lerp(originalPosition, destination, timer / moveOverTime);
+            yield return new WaitForFixedUpdate();
+            timer += Time.fixedDeltaTime;
+        }
+        transform.position = destination;
+    }
+}
